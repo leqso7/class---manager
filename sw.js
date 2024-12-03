cb5cf4537fa6c49dd91369206c6ece095b5db619
leqso7 <115240888+leqso7@users.noreply.github.com>
2024-12-03 17:09:50 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,26 @@
+const cacheName = 'site-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+];
+
 self.addEventListener('install', (event) => {
-    console.log('Service Worker: დაინსტალირდა');
-  });
-  self.addEventListener('fetch', (event) => {
-    console.log('Fetching:', event.request.url);
-  });
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        console.log('Caching all files');
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
