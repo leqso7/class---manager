4eb8577da726ce1f038708a69107981d42ac13ac
leqso7 <115240888+leqso7@users.noreply.github.com>
2024-12-03 16:53:36 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: დაინსტალირდა');
+  });
+  self.addEventListener('fetch', (event) => {
+    console.log('Fetching:', event.request.url);
+  });
